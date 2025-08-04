0ad3050e00d13faf9cd916eb7635062e13968cd3
mk2464-mak <220140107019kanzariyamayur@gmail.com>
2025-08-04 07:00:47 +0000

    mk

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[7] =
+static const flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    3,    1,    2,    0
+        0,    0,    5,    3,    3,    0,    1,    0,    0,    1,
+        2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -371,7 +372,7 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[3] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1
+        1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[15] =
     {   0,
-        0,    0,    3,    4,    4,    4
+        0,    0,   13,   14,    2,    9,    0,    8,    6,    0,
+       14,   14,    6,    8
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[15] =
     {   0,
-        6,    1,    6,    6,    6,    0
+       12,    1,   12,   12,   12,   13,   14,   13,    8,   14,
+       12,    0,   12,   12
     } ;
 
-static const flex_int16_t yy_nxt[7] =
+static const flex_int16_t yy_nxt[19] =
     {   0,
-        4,    5,    6,    3,    6,    6
+        4,    4,    4,    5,    6,    7,    8,    8,   10,   11,
+        9,    9,   12,    3,   12,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_chk[7] =
+static const flex_int16_t yy_chk[19] =
     {   0,
-        1,    1,    3,    6,    6,    6
+        1,    1,    1,    1,    5,    5,   13,   13,   14,    9,
+        8,    6,    3,   12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,19 +441,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "cipher.l"
-#line 2 "cipher.l"
+#line 1 "comment.l"
+#line 2 "comment.l"
 #include <stdio.h>
-
-char shift(char c) {
-    if (c >= 'a' && c <= 'z')
-        return ((c - 'a' + 3) % 26) + 'a';
-    if (c >= 'A' && c <= 'Z')
-        return ((c - 'A' + 3) % 26) + 'A';
-    return c;
-}
-#line 451 "lex.yy.c"
-#line 452 "lex.yy.c"
+#line 448 "lex.yy.c"
+#line 449 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -665,9 +662,9 @@ YY_DECL
 		}
 
 	{
-#line 13 "cipher.l"
+#line 5 "comment.l"
 
-#line 671 "lex.yy.c"
+#line 668 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -694,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 7 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 4 );
+		while ( yy_base[yy_current_state] != 14 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -726,15 +723,29 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 14 "cipher.l"
-{ putchar(shift(yytext[0])); }
+#line 6 "comment.l"
+{ printf("Single-line comment: %s\n", yytext); }
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 15 "cipher.l"
+#line 7 "comment.l"
+{
+                        printf("Multi-line comment: %s\n", yytext);
+                    }
+	YY_BREAK
+case 3:
+/* rule 3 can match eol */
+YY_RULE_SETUP
+#line 10 "comment.l"
+; // Ignore everything else
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 11 "comment.l"
 ECHO;
 	YY_BREAK
-#line 738 "lex.yy.c"
+#line 749 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1031,7 +1042,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 7 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1059,11 +1070,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 7 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 6);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1739,11 +1750,11 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 15 "cipher.l"
+#line 11 "comment.l"
 
 
 int main() {
-    printf("Enter text to encrypt using Caesar Cipher:\n");
+    printf("Extracting comments from C code:\n");
     yylex();
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// This is a single-line comment
+int main() {
+    /* This is a 
+       multi-line comment */
+    printf("Hello, World!\n"); 
+    printf("Hello, World!\n"); 
+    printf("Hello, World!\n"); 
+    printf("Hello, World!\n"); // Another single-line
+    return 0;
+}
